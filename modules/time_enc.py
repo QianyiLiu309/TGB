9cc309dc9b535d484a3c9765454e94c4c3c1307f
zzb <40760215+zzbuzzard@users.noreply.github.com>
2024-02-29 21:44:10 +0000

    Add multipliers to time encoding

--- modules/time_enc.py
+++ modules/time_enc.py
@@ -12,28 +12,36 @@ from torch.nn import Linear
 
 
 class TimeEncoder(torch.nn.Module):
-    def __init__(self, out_channels: int):
+    def __init__(self, out_channels: int, mul=0.01):
         super().__init__()
         self.out_channels = out_channels
         self.lin = Linear(1, out_channels)
+        self.mul = mul
+        with torch.no_grad():
+            self.lin.weight *= mul
 
     def reset_parameters(self):
         self.lin.reset_parameters()
+        with torch.no_grad():
+            self.lin.weight *= self.mul
 
     def forward(self, t: Tensor) -> Tensor:
         return self.lin(t.view(-1, 1)).cos()
 
 
 class ExpTimeEncoder(torch.nn.Module):
-    def __init__(self, out_channels: int):
+    def __init__(self, out_channels: int, mul=0.01):
         super().__init__()
         self.out_channels = out_channels
         self.lin = Linear(1, out_channels, bias=False)
+        self.mul = mul
         with torch.no_grad():
-            self.lin.weight *= 0.1
+            self.lin.weight *= mul
 
     def reset_parameters(self):
         self.lin.reset_parameters()
+        with torch.no_grad():
+            self.lin.weight *= self.mul
 
     def forward(self, t: Tensor) -> Tensor:
         # [w1 t, w2 t, w3 t, ...]
@@ -43,15 +51,18 @@ class ExpTimeEncoder(torch.nn.Module):
 
 class GaussianTimeEncoder(torch.nn.Module):
     """Inspired by Gaussian PDF"""
-    def __init__(self, out_channels: int):
+    def __init__(self, out_channels: int, mul=0.01):
         super().__init__()
         self.out_channels = out_channels
         self.lin = Linear(1, out_channels, bias=True)
+        self.mul = mul
         with torch.no_grad():
-            self.lin.weight *= 0.1
+            self.lin.weight *= mul
 
     def reset_parameters(self):
         self.lin.reset_parameters()
+        with torch.no_grad():
+            self.lin.weight *= self.mul
 
     def forward(self, t: Tensor) -> Tensor:
         return torch.exp(-self.lin(t.view(-1, 1)) ** 2)
