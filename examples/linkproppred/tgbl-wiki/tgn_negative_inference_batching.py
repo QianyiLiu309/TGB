cace7d7daad2b79f6d4675238469fe04c327caba
liuqianyi <Qianyi Liu>
2024-03-08 23:45:59 +0000

    Fix batch size to be 1 during negative edge inference

--- examples/linkproppred/tgbl-wiki/tgn_negative_inference_batching.py
+++ examples/linkproppred/tgbl-wiki/tgn_negative_inference_batching.py
@@ -303,7 +303,7 @@ for run_idx in range(NUM_RUNS):
     # Small batch size unimportant for train/val
     train_loader = TemporalDataLoader(train_data, batch_size=200)
     val_loader = TemporalDataLoader(val_data, batch_size=200)
-    test_loader = TemporalDataLoader(test_data, batch_size=BATCH_SIZE)
+    test_loader = TemporalDataLoader(test_data, batch_size=1)
 
     # Ensure to only sample actual destination nodes as negatives.
     min_dst_idx, max_dst_idx = int(data.dst.min()), int(data.dst.max())
