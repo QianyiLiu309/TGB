20bd8c6d8e45f7a0cf228fc03d6c406b487e4273
zzb <40760215+zzbuzzard@users.noreply.github.com>
2024-03-11 15:45:21 +0000

    Add FFT to dataset_plot.py

--- examples/linkproppred/tgbl-wiki/dataset_plot.py
+++ examples/linkproppred/tgbl-wiki/dataset_plot.py
@@ -11,25 +11,16 @@ import sys
 
 sys.path.append("..")
 
-import timeit
-
-import os
-import os.path as osp
-from pathlib import Path
 import numpy as np
-
 import torch
-
 from torch_geometric.loader import TemporalDataLoader
-
-# internal imports
 from tgb.utils.utils import get_args, set_random_seed, save_results
 from tgb.linkproppred.dataset_pyg import PyGLinkPropPredDataset
-
 from matplotlib import pyplot as plt
 from tqdm import tqdm
-
 from plot_utils import get_temporal_edge_times
+from sklearn.neighbors import KernelDensity
+import math
 
 
 args, _ = get_args()
@@ -69,4 +60,39 @@ np.save(f"../dataset_stats/{args.data}_gaps.npy", all_gaps)
 
 # counts = {i: test_pairs.count(i) for i in set(test_pairs)}
 # biggest = sorted(set(test_pairs), key=lambda i: counts[i], reverse=True)
+
+
+## DFT
+print("DFT Time")
+data_range = np.max(all_gaps) - np.min(all_gaps)
+# Fit PDF to data
+kde = KernelDensity(bandwidth=data_range/100)
+kde.fit(all_gaps[:, None])
+num_samples = 5000
+sample_spacing = data_range / num_samples
+# Sample PDF at linearly separated points
+x = np.linspace(min(all_gaps), max(all_gaps), num_samples)[:, None]
+log_dens = kde.score_samples(x)
+# Plot approximated PDF
+plt.fill_between(x[:, 0], np.exp(log_dens))
+plt.yscale('log')
+plt.show()
+# Apply FFT
+signal = log_dens  # working with log leads to less numerical errors but same output
+n = signal.shape[0]
+fft = np.fft.fft(signal)[:n//2]  # n//2 to discard negative frequencies
+# Get top frequencies
+amplitudes = np.abs(fft)
+freqs = np.fft.fftfreq(n, d=sample_spacing)[:n//2]  # corresponding freqs
+F = freqs[np.argsort(-amplitudes)]  # freqs sorted by increasing amplitude
+
+weights = 2 * math.pi * F
+
+print(f"Top periods in hours:")
+print((1 / F[:100]) / 60 / 60)
+print("Top 100 weights:")
+print(weights[:100])
+
+print(f"Saving dataset data to ../dataset_stats/{args.data}_frequencies_weight.npy")
+np.save(f"../dataset_stats/{args.data}_frequencies_weight.npy", weights)
 
