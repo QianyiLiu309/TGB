9a1b2a145e754df6cb770d46856037cde84594be
liuqianyi <Qianyi Liu>
2024-03-13 03:52:53 +0000

    Change default time_step for canparl

--- evaluate_checkpoints.sh
+++ evaluate_checkpoints.sh
@@ -7,7 +7,6 @@ TIME_ENCODER_VALUES=("learned_cos" "fixed_gaussian" "scaled_fixed" "scaled_fixed
 # Fixed values for other parameters
 num_run=5
 seed=1
-time_step=50
 edge_step=1
 model="DyRep"
 mul=1.0
@@ -15,6 +14,14 @@ mul=1.0
 # Iterate over the combinations of time_encoder and data arguments
 for time_encoder in "${TIME_ENCODER_VALUES[@]}"; do
   for data in "${DATASET_VALUES[@]}"; do
+    # Set the initial time_step value
+    time_step=50
+    
+    # Check if the dataset is "tgbl-canparl" and adjust time_step accordingly
+    if [[ "$data" == "tgbl-canparl" ]]; then
+      time_step=86400
+    fi
+
     # Call the Python script with the current combination of arguments
     python examples/linkproppred/tgbl-wiki/total_variation_eval.py \
       --data "$data" \
